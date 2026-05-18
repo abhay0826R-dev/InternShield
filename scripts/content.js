483e19e44974246718e4b0864d18fee8494bb5cb
abhay0826R-dev <abhay0826r@gmail.com>
2026-05-18 16:49:37 +0530

    changed name

--- scripts/content.js
+++ scripts/content.js
@@ -2,15 +2,6 @@
 function removeInternships() {
     let removed_internships_array = []
     let remove_internship_array = ["Across The Globe", "Zdminds", "She Can Foundation", "InAmigos Foundation"]
-    
-    // for (let i=0; i<50; i++)
-    // {
-    //     let x = document.querySelector(".individual_internship")
-    //     if (remove_internship_array.some(name => x.innerHTML.includes(name))) {
-    //         x.remove()
-    //         console.log("GOt It")
-    //     }
-    // }
 
     let x = document.querySelectorAll(".individual_internship")
     x.forEach(elem => {
